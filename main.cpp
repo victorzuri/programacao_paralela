318df222fffd58416b5a0c7cbf2b02e00e29534d
Victor Alvarez <victor.alvarez@tisafe.com>
2024-04-22 17:01:43 -0300

    Mudanças no Codigo

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include<stdio.h>
 #include<pthread.h>
 
-int main(){
-    printf("print funcionando!!\n");
+void *sample(void *) { 
+    printf("teste\n");
+    return NULL; 
+}
+
+int main() {
+    
+    pthread_t tid;
+    pthread_create(&tid, NULL, &sample, NULL); 
+    pthread_join(tid, NULL);
     return 0;
 }
